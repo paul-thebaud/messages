0ffe59e195fceed7262c81283eea040cd558f5db
Killian Hascoët <killianh@live.fr>
2019-02-11 14:51:15 +0100

    try

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,10 +30,10 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_WS_HOST,
-    wsPort: process.env.MIX_WS_PORT,
     wssHost: process.env.MIX_WSS_HOST,
     wssPort: process.env.MIX_WSS_PORT,
+    wsHost: process.env.MIX_WS_HOST,
+    wsPort: process.env.MIX_WS_PORT,
     encrypted: process.env.MIX_WS_ENCRYPTION,
     disableStats: true,
     auth: {
